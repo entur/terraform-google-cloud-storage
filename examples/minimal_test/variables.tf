b59b239fff2bdfb8f109e59b7052bb7287c1a51c
Bjørn Vestli <bjorn.vestli@entur.org>
2022-11-17 15:04:51 +0100

    test: add tests
    This will add unit and integration tests using terratest

--- examples/minimal_test/variables.tf
+++ examples/minimal_test/variables.tf
@@ -22,3 +22,9 @@ variable "init" {
 variable "generation" {
   default = 1
 }
+
+variable "name_override" {
+  description = "Set to override the default bucket name. Follows contentions; setting it to 'foo' in dev will result in the bucket being named 'ent-gcs-foo-dev-001' (<prefix>-<var.name_override>-<env>-<generation>). Is also applied to the name of the Kubernetes config map."
+  type        = string
+  default     = null
+}
---
+++ tests/cloud_storage_unit_test.go
@@ -0,0 +1,28 @@
+//go:build unit
+
+package cloud_storage_unit_test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/logger"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTerraformCloudStorageUnit(t *testing.T) {
+	// Construct the terraform options with default retryable errors to handle the most common
+	// retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// Set the path to the Terraform code that will be tested.
+		TerraformDir: "../examples/minimal_test",
+
+		// Disable colors in Terraform commands so its easier to parse stdout/stderr
+		NoColor: true,
+		Logger:  logger.Discard,
+	})
+
+	output := terraform.InitAndPlan(t, terraformOptions)
+
+	assert.Contains(t, output, "3 to add, 0 to change, 0 to destroy", "Plan OK and should attempt to create 3 resources")
+}
