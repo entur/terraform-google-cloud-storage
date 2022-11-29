ebc53605d717c1329e92f1fc92b34aafbd0860b4
Bjørn Vestli <33520068+bvestli@users.noreply.github.com>
2022-11-29 13:28:58 +0100

    test: add tests
    * test: add tests
    
    This will add unit and integration tests using terratest

--- examples/minimal_test/main.tf
+++ examples/minimal_test/main.tf
@@ -9,7 +9,7 @@ module "init" {
   # This is an example only; if you're adding this block to a live configuration,
   # make sure to use the latest release of the init module, found here:
   # https://github.com/entur/terraform-google-init/releases
-  source      = "github.com/entur/terraform-google-init//modules/init?ref=v1.1.0"
+  source      = "github.com/entur/terraform-google-init//modules/init?ref=v0.3.0"
   app_id      = "tfmodules"
   environment = "dev"
 }
@@ -22,7 +22,6 @@ module "cloud-storage" {
   source = "../../modules/bucket"
   init   = module.init
   #init       = var.init
-  generation = random_integer.random_revision_generation.result
-  location   = "EU"
-  #location   = "EUROPE-WEST1+EUROPE-WEST4"
+  generation = var.generation != null ? var.generation : random_integer.random_revision_generation.result
+  name_override = var.name_override != null ? var.name_override : var.init.app.id
 }
---
+++ tests/cloud_storage_unit_test.go
@@ -0,0 +1,47 @@
+//go:build unit
+
+package cloud_storage_unit_test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/logger"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTerraformCloudStorageUnit(t *testing.T) {
+	// Construct the terraform options with default retryable errors to handle the most common
+	// retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// Set the path to the Terraform code that will be tested.
+		TerraformDir: "fixtures/bucket",
+
+		// Disable colors in Terraform commands so its easier to parse stdout/stderr
+		NoColor: true,
+		Logger:  logger.Discard,
+
+		PlanFilePath: "plan.out",
+	})
+
+	plan := terraform.InitAndPlanAndShowWithStruct(t, terraformOptions)
+
+	// Make sure the right address vas registered
+	terraform.RequireResourceChangesMapKeyExists(t, plan, "module.cloud-storage.google_storage_bucket.main")
+
+	// Check values from plan output
+	cloudStorageChanges := plan.ResourcePlannedValuesMap["module.cloud-storage.google_storage_bucket.main"]
+	// Location
+	assert.Equal(t, cloudStorageChanges.AttributeValues["location"], "EUROPE-WEST1", "Incorrect location")
+	// force destroy defalts to false
+	assert.Equal(t, cloudStorageChanges.AttributeValues["force_destroy"], false, "Force destroy should be false")
+	// storage class is standard
+	assert.Equal(t, cloudStorageChanges.AttributeValues["storage_class"], "STANDARD", "Storage class should be type STANDARD")
+	// label for disable_offsite_backup should not exist
+	labels := cloudStorageChanges.AttributeValues["labels"]
+	disable_offsite_backup := labels.(map[string]interface{})["disable_offsite_backup"]
+	assert.Emptyf(t, disable_offsite_backup, "Found label disable_offsite_backup. This should only exist when set in a production environment.")
+	// object versioning is active
+	versioningEnabled := cloudStorageChanges.AttributeValues["versioning"].([]interface{})[0].(map[string]interface{})["enabled"]
+	assert.Equal(t, versioningEnabled, true, "Versioning is not enabled")
+}
