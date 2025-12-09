4b5a60ba2fd3886407d3222e8ed2d935457b00bd
Pavlo <pavlo.rosiuk@entur.org>
2025-12-09 15:22:11 +0100

    lock k8s provider to update minor/patch versions only

--- modules/bucket/versions.tf
+++ modules/bucket/versions.tf
@@ -3,6 +3,10 @@ terraform {
     google = {
       source  = "hashicorp/google"
       version = ">=4.26.0"
+    },
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.38"
     }
   }
   required_version = ">=0.13.2"
