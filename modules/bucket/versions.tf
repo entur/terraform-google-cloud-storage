3f24db4b2a3ac3bb427146e2b05fd78a8b26057a
Pavlo <pavlo.rosiuk@entur.org>
2025-12-09 15:29:56 +0100

    fix: removed coma

--- modules/bucket/versions.tf
+++ modules/bucket/versions.tf
@@ -3,10 +3,10 @@ terraform {
     google = {
       source  = "hashicorp/google"
       version = ">=4.26.0"
-    },
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.38"
+      version = "~> 2.0"
     }
   }
   required_version = ">=0.13.2"
